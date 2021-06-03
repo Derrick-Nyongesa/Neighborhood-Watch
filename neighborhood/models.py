2218d19fb213fde3beead2d8903b9affcde07bcc
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-03 07:36:24 +0300

    Created a neighborhood Model

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighborhood')
+    logo = CloudinaryField('image',null=True, blank=True)
+    health_number = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} neighbhood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def search_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
