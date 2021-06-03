8b227084a8042321726601b6fb31be39f503b716
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-03 07:47:54 +0300

    Created a UserProfile Model

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -25,3 +25,28 @@ class NeighbourHood(models.Model):
     @classmethod
     def search_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=120, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = CloudinaryField('image',null=True, default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='ocupants', blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+
