01d76393aa6d49c3e5a0aec94df65edc24cbfad9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-05 13:52:24 +0300

    added django secret key to .env file

--- watch/settings.py
+++ watch/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9m+s8x1_dw37kcs4ixz9#7hmv7*i9w_21p%%1_wt#2-2cdu!+c'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
